861622b764902b411d52e0ca2b014f0f178d780d
Davide Andreoli <davide.andreoli@icloud.com>
2023-01-20 23:49:05 +0100

    Added spark instance to docker compose.

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,11 @@ FROM apache/airflow:2.5.0
 
 ENV AIRFLOW_HOME=/opt/airflow
 
+USER root
+RUN apt-get update -qq && apt-get install vim wget default-jre -qqq
+
+USER $AIRFLOW_UID
+
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-USER $AIRFLOW_UID
--- dags/get_cases_data.py
+++ dags/get_cases_data.py
@@ -5,25 +5,34 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
 
+from merge_into_month import merge_into_month
+
 AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 
 URL_PREFIX = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/' 
 URL_TEMPLATE = URL_PREFIX + '/dpc-covid19-ita-andamento-nazionale-{{ execution_date.strftime(\'%Y%m%d\') }}.csv'
-OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + '/output_{{ execution_date.strftime(\'%Y%m%d\') }}.csv'
+OUTPUT_FILE_TEMPLATE = 'raw_cases_{{ execution_date.strftime(\'%Y%m%d\') }}.csv'
 
 with DAG(
     dag_id="get_cases_data",
-    start_date=datetime(2023,1,1),
+    start_date=datetime(2022,12,20),
     end_date=datetime(2023,1,10),
     schedule_interval="@daily") as dag:
 
     start_task = DummyOperator(task_id='start')
 
-    wget_task = BashOperator(
-        task_id='wget',
-        bash_command=f'echo {OUTPUT_FILE_TEMPLATE}'
+    download_data_task = BashOperator(
+        task_id='download_data',
+        bash_command=f'curl -sSL -o {OUTPUT_FILE_TEMPLATE} {URL_TEMPLATE}'
+    )
+
+    merge_into_month_task = PythonOperator(
+        task_id="ingest",
+        python_callable=merge_into_month,
+        op_kwargs={},
     )
 
     end_task = DummyOperator(task_id='end')
 
-    start_task >> wget_task >> end_task
+    start_task >> download_data_task >> merge_into_month_task >> end_task
+
