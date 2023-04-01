e02407e022dd7eb27565f3d5f18999a1f0f11579
Davide Andreoli <davide.andreoli@icloud.com>
2023-04-01 14:56:28 +0200

    Added database insert

--- spark/app/merge_data.py
+++ spark/app/merge_data.py
@@ -6,6 +6,9 @@ from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 
+DB_URL = "jdbc:postgresql://warehouse-db:5432/airflow"
+DB_USER = 'airflow'
+DB_PASSWORD = 'airflow'
 
 spark = SparkSession.builder.appName("merge_data").getOrCreate()
 
@@ -31,6 +34,15 @@ df = df.withColumn("id",F.md5(F.concat(F.col("date"),F.col("country_cod"))))
 # Select only relevant colums
 
 df = df.select("id","date","country_cod","new_positive_cases")
+
+# Delete data of same day first
+
+df.write.mode("append").format("jdbc") \
+    .option("url", DB_URL) \
+    .option("driver", "org.postgresql.Driver") \
+    .option("dbtable", "covid_cases") \
+    .option("user", DB_USER) \
+    .option("password", DB_PASSWORD).save()
 
 
 
