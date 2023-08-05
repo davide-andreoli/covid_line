69db803b3cd27ce9d375c31da19e200af4979953
Davide Andreoli <davide.andreoli@icloud.com>
2023-08-05 22:06:08 +0200

    Read from parquet

--- spark/app/save_to_warehouse.py
+++ spark/app/save_to_warehouse.py
@@ -9,6 +9,8 @@ from pyspark.sql import functions as F
 home_dir = os.path.expanduser('~')
 print(os.listdir(home_dir + '/spark'))
 
+execution_date = datetime.strptime(os.environ.get("AIRFLOW_CTX_EXECUTION_DATE").split('T')[0], '%Y-%m-%d')
+
 spark = SparkSession \
     .builder \
     .appName("save_to_warehouse") \
@@ -19,11 +21,19 @@ spark = SparkSession \
     #.config("spark.jars", home_dir + '/spark' + "/postgresql-42.5.4.jar") \
     #    .config("spark.sql.warehouse.dir", "/hive/warehouse/dir")
 
-df = spark.read.table("pokes")
+df = spark.read \
+    .parquet(f"/usr/share/covid_data/pq/{execution_date.strftime('%Y')}/{execution_date.strftime('%m')}/cases_{execution_date.strftime('%Y%m%d')}.parquet")
 
 df.printSchema()
 
-execution_date = datetime.strptime(os.environ.get("AIRFLOW_CTX_EXECUTION_DATE").split('T')[0], '%Y-%m-%d')
+if spark.catalog.tableExists("cases"):
+    print("exists")
+    # insert into temp table and merge
+else:
+    # create table
+    print("no")
+
+
 
 # .master("spark://spark:7077") ? --> find a way to connect to Spark cluster
 
