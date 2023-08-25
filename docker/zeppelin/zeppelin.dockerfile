1e06bd312d2faf53656a168fc22c7b6e0bc20201
Davide Andreoli <116097137+davide-andreoli@users.noreply.github.com>
2023-08-25 18:05:06 +0200

    Dockerfile update (#6)
    * Moved spark to bitnami
    
    * Updated container name
    
    * moved superset files
    
    * Cleaned airflow script
    
    * Restored postgres healtcheck
    
    * Moved zeppelin files
    
    * removed spaces
    
    * Hadoop to Apache Image
    
    * Moved hive to official
    
    * fixed hadoop persistence
    
    * Moved folders out of temp
    
    * Updated readme
    
    * Updated readme
    
    * Added configurations
    
    * Removed superset from piepline
    
    * Removed superet fom pipelin
    
    * delete file
    
    * scratch dire on hdfs
    
    * hive setup
    
    * Bumped pyspark version
    
    * Minor fixes
    
    * Changed pyspark version
    
    * Changed notebook location

--- docker/zeppelin/zeppelin.dockerfile
+++ docker/zeppelin/zeppelin.dockerfile
@@ -1,7 +1,7 @@
 FROM apache/zeppelin:0.10.1
 
 ENV BASE_URL=https://archive.apache.org/dist/spark/
-ENV SPARK_VERSION=3.1.2
+ENV SPARK_VERSION=3.1.3
 ENV HADOOP_VERSION=3.2
 
 USER root
@@ -15,7 +15,11 @@ RUN wget -nc ${BASE_URL}/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-hadoo
 
 RUN apt-get update && apt-get install -y python3
 
-RUN pip install pyspark==3.1.2 findspark ipython
+# Install python 3.9 with pyenv and make it global
+
+RUN pip install pyspark==3.1.3 findspark ipython
+
+
 
 ENV SPARK_HOME "/spark"
 ENV PYSPARK_PYTHON "/usr/bin/python3.8"
--- spark/app/save_to_warehouse.py
+++ spark/app/save_to_warehouse.py
@@ -4,16 +4,14 @@ from datetime import datetime
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 
-home_dir = os.path.expanduser('~')
-print(os.listdir(home_dir + '/spark'))
-
 execution_date = datetime.strptime(os.environ.get("AIRFLOW_CTX_EXECUTION_DATE").split('T')[0], '%Y-%m-%d')
 
 spark = SparkSession \
     .builder \
     .appName("save_to_warehouse") \
-    .config("spark.sql.warehouse.dir", "/user/hive/warehouse") \
+    .config("hive.exec.scratchdir", "hdfs://namenode:8020/opt/hive/scratch_dir") \
     .config("hive.metastore.uris", "thrift://hive-metastore:9083") \
+    .config("spark.sql.warehouse.dir", "/user/hive/warehouse") \
     .enableHiveSupport() \
     .getOrCreate()
     # TO DO: configure DB covid_data
@@ -21,9 +19,8 @@ spark = SparkSession \
 df = spark.read \
     .parquet(f"hdfs://namenode:8020/covid_data/pq/{execution_date.strftime('%Y')}/{execution_date.strftime('%m')}/{execution_date.strftime('%d')}/cases_{execution_date.strftime('%Y%m%d')}.parquet")
 
-
-
-if spark.catalog.tableExists("cases"):
+if "cases" in spark.catalog.listTables():
+    print("i'm not here")
     current_table = spark.read.table("cases")
     # Renoves matching rows from current table
     current_table = current_table.join(df, current_table.collection_id == df.collection_id, "leftanti")
