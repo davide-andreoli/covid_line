13c157fd0ef2c83ea1f6cb7a1c85664b4b514d5d
Davide Andreoli <116097137+davide-andreoli@users.noreply.github.com>
2023-08-26 09:40:18 +0200

    Minor cleanup (#7)
    * Cleaned folder
    
    * fixed pipeline
    
    * Updated readme

--- utils/graph.py
+++ utils/graph.py
@@ -1,12 +1,30 @@
-from diagrams import Diagram
+from diagrams import Diagram, Cluster
 from diagrams.onprem.vcs import Github
 from diagrams.generic.storage import Storage
 from diagrams.onprem.analytics import Spark, Hive, Superset, Hadoop
+from diagrams.programming.language import Python
+from diagrams.onprem.workflow import Airflow
+from diagrams.custom import Custom
 
 graph_attr = {
     "label": ""
 }
 
 with Diagram("Pipeline", show=False, graph_attr=graph_attr):
-    Github("Protezione Civile") >> Storage("Local CSV") >> Spark("Merge data") >> Hadoop("Parquet on HDFS") >> Spark("Save to warehouse") >> Hive("Data warehouse") >> Superset("Data visualization")
+    airflow = Airflow("Orchestrator")
+    with Cluster("Pipeline"):
+        source = Github("Protezione Civile")
+        storage = Storage("Local CSV")
+        merge_data = Spark("Merge data")
+        hadoop_storage = Hadoop("Parquet on HDFS")
+        save_to_warehouse = Spark("Save to warehouse")
+        hive_warehouse = Hive("Data warehouse")
+        superset = Superset("Data visualization")
+        zeppelin = Custom("Data exploration", "./logos/zeppelin_classic_logo.png")
+        source >> storage >> merge_data >> hadoop_storage >> save_to_warehouse >> hive_warehouse
+        hive_warehouse >> superset
+        hive_warehouse >> zeppelin
+    airflow >> source
+
+    
 
